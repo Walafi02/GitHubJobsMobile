e2e15e2bc3833e7327ef71902d567ea4cadb51eb
Walafi02 <walafif@yahoo.com>
2020-08-13 12:12:43 -0300

    Add eslint and prettier

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import {View, Text} from 'react-native';
+
+import {Dashboard} from '~/pages';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return <Dashboard />;
+};
+
+export default App;
